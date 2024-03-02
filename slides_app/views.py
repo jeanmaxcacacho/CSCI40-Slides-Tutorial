4572652865521eea50250ca1fb3fddacb8aa0995
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-03-02 12:38:09 +0800

    Arianna go kys

--- slides_app/views.py
+++ slides_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpReponse
+from django.http import HttpResponse
 
 # Create your views here.
 
-
 def index(request):
     return HttpResponse("go fucking kill yourself || from slides_app")
+
+
+def template_context_view(request):
+    return render(request, 'template.html', {'name':'slavery was a choice -Kanye West'})
