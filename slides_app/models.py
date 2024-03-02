7693c3f4b9794641dabaa882deb5f9b8e787ba51
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-03-02 18:54:20 +0800

    modelsxsz

--- slides_app/models.py
+++ slides_app/models.py
@@ -1,3 +1,31 @@
+from datetime import datetime
+
+from django.urls import reverse
 from django.db import models
 
-# Create your models here.
+
+class TaskGroup(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    due_date = models.DateTimeField(null=False)
+    taskgroup = models.ForeignKey(
+            TaskGroup,
+            on_delete=models.CASCADE,
+            default=1
+            )
+
+
+    def __str__(self):
+        return '{}: due on {}'.format(self.name, self.due_date)
+
+
+    def get_absolute_url(self):
+        return reverse('task_detail', args=[str(self.name)])
+
+
+    @property
+    def is_due(self):
+        return datetime.now() >= self.due_date
